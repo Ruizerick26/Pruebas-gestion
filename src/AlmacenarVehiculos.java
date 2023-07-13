3f08f9ebcfd185900ef0a9f3d46a85b72a330404
Ruizerick26 <ruizerick2525@gmail.com>
2023-07-12 20:10:20 -0500

    mejoras prueba caja blanca

--- src/AlmacenarVehiculos.java
+++ src/AlmacenarVehiculos.java
@@ -1,50 +1,69 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class AlmacenarVehiculos {
     public static void main(String[] args) {
         // Crear los objetos de vehículo
-        Vehiculo vehiculo1 = new Vehiculo("Toyota", "Corolla", 2022);
-        Vehiculo vehiculo2 = new Vehiculo("Honda", "Civic", 2021);
-        Vehiculo vehiculo3 = new Vehiculo("Ford", "Mustang", 2023);
+        Vehiculo vehiculo1 = new Vehiculo();
+        Vehiculo vehiculo2 = new Vehiculo();
+        Vehiculo vehiculo3 = new Vehiculo();
 
         // Array de vehículos
         Vehiculo[] vehiculos = {vehiculo1, vehiculo2, vehiculo3};
 
         // Nombre del archivo
-        String nombreArchivo = "vehiculos.txt";
+        String nombres[]={"archivo1.txt","archivo2.txt","archivo3.txt"};
 
-        try {
-            // Crear un ObjectOutputStream para escribir objetos en el archivo
-            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(nombreArchivo));
-
-            // Escribir el array de vehículos en el archivo
-            outputStream.writeObject(vehiculos);
-
-            // Cerrar el ObjectOutputStream
-            outputStream.close();
-
-            System.out.println("La información de los vehículos se ha almacenado en el archivo " + nombreArchivo);
-        } catch (IOException e) {
-            System.out.println("Error al escribir en el archivo: " + e.getMessage());
+        Scanner rc = new Scanner(System.in);
+        String modelo;
+        String marca;
+        String anio;
+        for (int i = 0; i<vehiculos.length;i++){
+            System.out.println("Ingrese La marca del vehiculo "+ (i+1));
+            marca = rc.nextLine();
+            vehiculos[i].setMarca(marca);
+            System.out.println("Ingrese el modelo del vehiculo "+ (i+1));
+            modelo = rc.nextLine();
+            vehiculos[i].setModelo(modelo);
+            System.out.println("Ingrese el año del vehiculo "+ (i+1));
+            anio = rc.nextLine();
+            vehiculos[i].setAnio(anio);
         }
+        for(int i=0;i<nombres.length;i++) {
+            try {
+                // Crear un ObjectOutputStream para escribir objetos en el archivo
+                ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(nombres[i]));
 
-        // Leer la información de los vehículos desde el archivo
-        try {
-            // Crear un ObjectInputStream para leer objetos desde el archivo
-            ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(nombreArchivo));
+                // Escribir el array de vehículos en el archivo
+                outputStream.writeObject(vehiculos[i]);
 
-            // Leer el array de vehículos desde el archivo
-            Vehiculo[] vehiculosLeidos = (Vehiculo[]) inputStream.readObject();
+                // Cerrar el ObjectOutputStream
+                outputStream.close();
 
-            // Cerrar el ObjectInputStream
-            inputStream.close();
-
-            // Mostrar la información de los vehículos leídos
-            for (Vehiculo vehiculo : vehiculosLeidos) {
-                System.out.println(vehiculo);
+                System.out.println("La información de los vehículos se ha almacenado en el archivo " + nombres[i]);
+            } catch (IOException e) {
+                System.out.println("Error al escribir en el archivo: " + e.getMessage());
             }
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Error al leer el archivo: " + e.getMessage());
+
+
+            // Leer la información de los vehículos desde el archivo
+            try {
+                // Crear un ObjectInputStream para leer objetos desde el archivo
+                ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(nombres[i]));
+
+                // Leer el array de vehículos desde el archivo
+                Vehiculo[] vehiculosLeidos = (Vehiculo[]) inputStream.readObject();
+
+                // Cerrar el ObjectInputStream
+                inputStream.close();
+
+                // Mostrar la información de los vehículos leídos
+                for (Vehiculo vehiculo : vehiculosLeidos) {
+                    System.out.println(vehiculo);
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("Error al leer el archivo: " + e.getMessage());
+            }
         }
     }
 }
--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -4,12 +4,39 @@ import java.io.*;
 public class Vehiculo implements Serializable {
     private String marca;
     private String modelo;
-    private int anio;
+    private String anio;
 
-    public Vehiculo(String marca, String modelo, int anio) {
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public void setAnio(String anio) {
+        this.anio = anio;
+    }
+
+    public Vehiculo(String marca, String modelo, String anio) {
         this.marca = marca;
         this.modelo = modelo;
         this.anio = anio;
+    }
+
+    public Vehiculo() {
     }
 
     @Override
